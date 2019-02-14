89d125be5d1619244709dc2f4e59dc029bfe6208
WoodyHouWang <wanghou92@gmail.com>
2019-02-13 18:52:26 -0800

    add agent

---
+++ Src/Qlearning/agent.h
@@ -0,0 +1,68 @@
+/**
+*	Author: Hou Wang
+*   PID: A53241783
+*	This file defines the data structure for agent
+*/
+
+#pragma once
+
+extern const int TABLE_SIZE;
+extern const int NUM_ACTIONS;
+
+enum Actions {
+};
+
+struct Agent
+{
+	enum Action {
+		RIGHT = 0,
+		BOTTOM = 1,
+		LEFT = 2,
+		TOP = 3
+	};
+
+	~Agent() {
+		if (m_h_qtable) {
+			for (int i = 0; i < table_size; ++i) {
+				if (m_h_qtable[i]) {
+					for (int j = 0; j < table_size; ++j) {
+						if (m_h_qtable[i][j]) {
+							delete m_h_qtable[i][j];
+							m_h_qtable[i][j] = nullptr;
+						}
+					}
+
+					delete m_h_qtable[i];
+					m_h_qtable[i] = nullptr;
+				}
+			}
+
+			delete m_h_qtable;
+			m_h_qtable = nullptr;
+		}
+	}
+	
+	Agent(int s = TABLE_SIZE, int n = NUM_ACTIONS):table_size(s),num_actions(n) {
+		learningRate = 0.1;
+		resetAction();
+		epsilon_init();
+		qtable_init(table_size, num_actions);
+	}
+
+	void qtable_init(int table_size = TABLE_SIZE, int num_actions = NUM_ACTIONS);
+	void epsilon_init() {
+		this->m_epsilon = 1.0f;
+	}
+	
+	void resetAction() {
+		this->m_action = -1;
+	}
+	short getMaxQActionVal(int2* state);
+
+	float m_epsilon;
+	float*** m_h_qtable;
+	int m_action;
+	int table_size;
+	int num_actions
+	float learningRate;
+};
--- Src/Qlearning/qlearningImpl.cu
+++ Src/Qlearning/qlearningImpl.cu
@@ -8,19 +8,85 @@
 #include <curand_kernel.h>
 #include <helper_cuda.h>
 #include <helper_functions.h>
+#include <stdlib.h>
 #include "draw_env.h"
+#include "agent.h"
 #include "common_def.h"
 
+const int TABLE_SIZE = 4;
+const int NUM_ACTIONS = 4;
+
+// define number of agents 
+const int NUM_AGENTS = 1;
+const Agent* agents;
+
+// Implementation of Agent
+void Agent::qtable_init(int table_size, int num_actions) {
+	this->m_h_qtable = new int**[table_size];
+	for (int i = 0; i < table_size; ++i) {
+		this->m_h_qtable[i] = new int*[table_size];
+		for (int j = 0; j < num_actions; ++j) {
+			this->m_h_qtable[i][j] = new int[num_actions];
+			memset(this->m_h_qtable[i][j], 0, num_actions);
+		}
+	}
+}
+
+short Agent::getMaxQActionVal(int2* state) {
+	int x = state->x;
+	int y = state->y;
+
+	short cand = RIGHT;
+	for (int i = RIGHT; i <= TOP; ++i) {
+		if (y == table_size - 1 && i == BOTTOM) continue;
+		if (y == 0 && i == TOP) continue;
+		if (x == table_size - 1 && i == RIGHT) {
+			++cand;
+			continue;
+		}
+		if (x == 0 && i == LEFT) continue;
+
+		cand = this->m_h_qtable[y][x][cand] >= this->m_h_qtable[y][x][i] ? cand : i;
+	}
+
+	return cand;
+}
+
+// Implemetation of required functions
 void agent_init() {
+	if (!agents) {
+		agents = new Agent;
+	}
 }
 
 void agent_clearaction() {
+	agents->resetAction();
 }
 
 float agent_adjustepsilon() {
+	agents.m_epsilon -= 0.01f;
+	return agents.m_epsilon;
 }
 
 short* agent_action(int2* cstate) {
+	float seed = (float)(rand() % 100) / 100.0f;
+	short ans = RIGHT;
+
+	if (seed < agents->m_epsilon) {
+		ans = rand() % 4;
+	}
+	else {
+		short ans = agents->getMaxQActionVal(cstate);
+	}
+
+	agents->m_action = ans;
+
+	return &ans;
 }
+
 void agent_update(int2* cstate, int2* nstate, float *rewards) {
+	float alpha = 0.5;
+	float delta = agents->getMaxQActionVal(nstate);
+	float curVal = agents->m_h_qtable[cstate->y][cstate->x][agents->m_action];
+	agents->m_h_qtable[cstate->y][cstate->x][agents->m_action] = curVal + alpha * (*(rewards) + agents->learningRate * delta - curVal);
 }
