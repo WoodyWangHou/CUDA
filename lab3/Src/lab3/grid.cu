cd1175d35ed9f23828f3b0446ca6eb341a21de11
WoodyHouWang <wanghou92@gmail.com>
2019-03-12 22:42:21 -0700

    complete lab3

--- lab3/Src/lab3/grid.cu
+++ lab3/Src/lab3/grid.cu
@@ -7,6 +7,7 @@
 #include <cuda_runtime.h>
 #include <helper_cuda.h>
 #include <string>
+#include <ctime>
 #include <cstdlib>
 #include "grid.h"
 namespace grid {
@@ -33,30 +34,24 @@ namespace grid {
 			#pragma unroll
 			for (int lane = 1; lane <= 32; lane *= 2) {
 				unsigned int tmp = __shfl_up(prefixOne, lane);
+				unsigned int zeroTmp = __shfl_up(prefixZero, lane);
 				if (idx >= lane) {
 					prefixOne += tmp;
+					prefixZero += zeroTmp;
 				}
 			}
-
-			// get prefix sums for zero
-			#pragma unroll
-			for (int lane = 1; lane <= 32; lane *= 2) {
-				unsigned int tmp = __shfl_up(prefixZero, lane);
-				if (idx >= lane) {
-					prefixZero += tmp;
-				}
-			}
+			 
+			int laneId = 31 % blockDim.x;
+			unsigned int totalZero = __shfl(prefixZero, laneId, blockDim.x);
 
 			if (zero > 0) {
-				// current bit is 0
-				s_keys_values[prefixZero][1] = curKey;
-				s_keys_values[prefixZero][0] = curVal;
+				// current bit is 
+				s_keys_values[prefixZero - 1][1] = curKey;
+				s_keys_values[prefixZero - 1][0] = curVal;
 			} else {
 				// current bit is 1
-				int laneId = 31 % blockDim.x;
-				int totalZero = __shfl(prefixZero, laneId, blockDim.x);
-				s_keys_values[prefixOne + totalZero][1] = curKey;
-				s_keys_values[prefixOne + totalZero][0] = curVal;
+				s_keys_values[prefixOne + totalZero - 1][1] = curKey;
+				s_keys_values[prefixOne + totalZero - 1][0] = curVal;
 			}
 
 			mask <<= 1;
@@ -67,15 +62,32 @@ namespace grid {
 		d_value[idx] = s_keys_values[idx][0];
 	}
 
-	__global__ void pCountParticles(int *d_keys, int *d_value, int *d_count) {
+	__global__ void pCountParticles(int *d_keys, int *d_cellId, int *d_count) {
+		int idx = threadIdx.x;
+		int laneId = idx & 0x1f;
+		int val = d_keys[idx];
+		int nval = __shfl_down(val, 1);
+		if (idx == 31) nval = 0;
+
+		int mask = __ballot(nval != val);
+		int offset = __popc(mask & ((1 << laneId) - 1));
+		int zcnt = __clz(mask & ((1 << laneId) - 1));
+		int runcnt = zcnt - 31 + laneId;
 		
+		if (nval != val) {
+			d_cellId[offset] = val;
+			d_count[offset] = runcnt;
+		}
 	}
 
 	void Grid::init() {
 		this->count = (int *)malloc(sizeof(int) * NUMBER_OF_KEYS);
 		this->cellId = (int *)malloc(sizeof(int) * NUMBER_OF_KEYS);
+		memset(this->cellId, -1, sizeof(int) * NUMBER_OF_KEYS);
 		this->keys = (int *)malloc(sizeof(int) * NUMBER_OF_PARTICLES);
 		this->value = (int *)malloc(sizeof(int) * NUMBER_OF_PARTICLES);
+
+		std::srand(time(NULL));
 
 		for (int i = 0; i < NUMBER_OF_PARTICLES; ++i) {
 			this->keys[i] = std::rand() % NUMBER_OF_KEYS;
@@ -85,8 +97,8 @@ namespace grid {
 	
 	void Grid::sortKeys() {
 		dim3 block(NUMBER_OF_PARTICLES);
-		int *d_keys;
-		int *d_value;
+		int *d_keys = NULL;
+		int *d_value = NULL;
 
 		CHECK(cudaMalloc((void **)&d_keys, sizeof(int) * NUMBER_OF_PARTICLES));
 		CHECK(cudaMalloc((void **)&d_value, sizeof(int) * NUMBER_OF_PARTICLES));
@@ -102,21 +114,17 @@ namespace grid {
 	void Grid::countParticles() {
 		dim3 block(NUMBER_OF_PARTICLES);
 		int *d_keys;
-		int *d_value;
 		int *d_count;
 		int *d_cellId;
 		
 		CHECK(cudaMalloc((void **)&d_keys, sizeof(int) * NUMBER_OF_PARTICLES));
-		CHECK(cudaMalloc((void **)&d_value, sizeof(int) * NUMBER_OF_PARTICLES));
 		CHECK(cudaMalloc((void **)&d_count, sizeof(int) * NUMBER_OF_KEYS));
 		CHECK(cudaMalloc((void **)&d_cellId, sizeof(int) * NUMBER_OF_KEYS));
 		CHECK(cudaMemcpy(d_keys, this->keys, sizeof(int) * NUMBER_OF_PARTICLES, cudaMemcpyHostToDevice));
-		CHECK(cudaMemcpy(d_value, this->value, sizeof(int) * NUMBER_OF_PARTICLES, cudaMemcpyHostToDevice));
 
-		pCountParticles << <1, block >> > (d_keys, d_value, d_count);
+		pCountParticles << <1, block >> > (d_keys, d_cellId, d_count);
 
 		CHECK(cudaMemcpy(this->keys, d_keys, sizeof(int) * NUMBER_OF_PARTICLES, cudaMemcpyDeviceToHost));
-		CHECK(cudaMemcpy(this->value, d_value, sizeof(int) * NUMBER_OF_PARTICLES, cudaMemcpyDeviceToHost));
 		CHECK(cudaMemcpy(this->count, d_count, sizeof(int) * NUMBER_OF_KEYS, cudaMemcpyDeviceToHost));
 		CHECK(cudaMemcpy(this->cellId, d_cellId, sizeof(int) * NUMBER_OF_KEYS, cudaMemcpyDeviceToHost));
 	}
@@ -126,10 +134,10 @@ namespace grid {
 			if(i != *(this->cellId)) {// empty cell
 				printf("%d %d \n", i, 0);
 			}else{
-				printf("%d %d" , *(this->cellId), ∗(this->count));
+				printf("%d %d " , *(this->cellId), ∗(this->count));
 				this->cellId++;
 				for (int k = 0; k < ∗(this->count); k++) {
-					printf("%2d", *(this->value));
+					printf("%2d ", *(this->value));
 					this->value++;
 				}
 				printf("\n");
